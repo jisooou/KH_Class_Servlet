448146bd9425d7eaf81263cd1767710fdbe78b8d
jisooou <87888800+jisooou@users.noreply.github.com>
2024-05-02 18:31:03 +0900

    Add files via upload
    prj12 업데이트

---
+++ prj12/src/main/java/com/kh/app/board/controller/BoardlnsertController.java
@@ -0,0 +1,57 @@
+package com.kh.app.board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.app.board.dao.BoardDao;
+import com.kh.app.board.vo.BoardVo;
+
+@WebServlet("/board/insert")
+public class BoardlnsertController extends HttpServlet{
+
+//	게시글 작성 (화면)
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/board/insert.jsp").forward(req, resp);
+	}
+	
+//	게시글 작성
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		데이터 꺼내기 
+		//	한글 처리
+		req.setCharacterEncoding("UTF-8");
+		String title = req.getParameter("title");
+		String content = req.getParameter("content");
+		
+//		데이터 뭉치기 
+		BoardVo vo = new BoardVo();
+		vo.setTitle(title);
+		vo.setContent(content);
+		
+		
+		int result = 0; 
+		try {
+			//	복잡한 작업
+			BoardDao dao = new BoardDao();
+			result = dao.insert(vo);		
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("DB에 insert 하다가 예외 발생 ~~~");
+		}
+
+		
+//		결과처리 (문자열내보내기)
+		String msg = "게시글 작성 성공!";
+		if(result != 1) {
+			msg = "게시글 작성 실패...";
+		}
+		req.setAttribute("msg", msg);
+		req.getRequestDispatcher("/WEB-INF/views/common/result.jsp").forward(req, resp);
+	}
+}
---
+++ prj12/src/main/java/com/kh/app/board/dao/BoardDao.java
@@ -0,0 +1,67 @@
+package com.kh.app.board.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kh.app.board.vo.BoardVo;
+import com.kh.app.db.JDBCTemplate;
+
+public class BoardDao {
+	
+//	게시글 작성 
+	public int insert(BoardVo vo) throws Exception {
+//		conn
+		Connection conn = JDBCTemplate.getConnection();
+		
+//		SQL
+		String sql = "INSERT INTO BOARD(TITLE, CONTENT) VALUES(?,?)";
+		
+//		pstmt
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, vo.getTitle());
+		pstmt.setString(2, vo.getContent());
+		int result = pstmt.executeUpdate();
+		
+		if(result == 1) {
+			conn.commit();			
+		}
+		
+//		result
+		return result; 
+		 
+	 }
+
+	public List<BoardVo> selectBoardList() throws Exception{
+//		conn
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String id = "C##KH";
+		String pwd = "1234";
+		Connection conn = DriverManager.getConnection(url, id, pwd);
+		conn.setAutoCommit(false);
+		
+//		SQL
+		String sql = "SELECT * FROM BOARD";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+		
+		List<BoardVo> voList = new ArrayList<BoardVo>();
+		while(rs.next()) {
+			String title = rs.getString("TITLE");
+			String content = rs.getString("CONTENT");
+			
+			BoardVo vo = new BoardVo();
+			vo.setTitle(title);
+			vo.setContent(content);
+			
+			voList.add(vo);
+		}
+		
+//		result
+		return voList; 
+	}
+}
