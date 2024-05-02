7018dc40f134fdf87a51bb43dad77b48156b3707
jisooou <87888800+jisooou@users.noreply.github.com>
2024-05-02 18:29:23 +0900

    Add files via upload
    prj11 업데이트

---
+++ prj11board/src/main/java/com/kh/app/board/controller/BoardInsertController.java
@@ -0,0 +1,45 @@
+package com.kh.app.board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/insert")
+public class BoardInsertController extends HttpServlet{
+	
+	
+//	게시글 작성(화면)
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/board/insert.jsp").forward(req, resp);
+	}
+	
+	
+//	게시글 작성
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		데이터 꺼내기
+		String title = req.getParameter("title");
+		String content = req.getParameter("content");
+		
+//		복잡한 작업
+//		int result = new BoardDao().insert(BoardVo객체);
+		
+//		실행결과(리턴값)가 1이 나왔다고 가정하고
+		int result = 1;
+		
+//		결과처리 (문자열내보내기)
+		if(result == 1) {
+			req.setAttribute("msg", "게시글 작성 성공!");
+		}else {
+			req.setAttribute("msg", "게시글 작성 실패...");
+		}
+		req.getRequestDispatcher("/WEB-INF/views/common/result.jsp").forward(req, resp);	
+		
+		
+	}
+}
